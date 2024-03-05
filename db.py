77207983919e836432a0153169805b2c5ee81089
ArthurChenEvans <arthurchenevans@protonmail.com>
2024-03-05 14:59:35 +0000

    Added table to store recalls

--- db.py
+++ db.py
@@ -1,13 +1,25 @@
 import sqlite3
 
 def setup_database():
-	con = sqlite3.connect("discord.db")
-	cur = con.cursor()
+    con = sqlite3.connect("discord.db")
+    cur = con.cursor()
 
-	cur.execute("""CREATE TABLE IF NOT EXISTS users(id INT PRIMARY KEY, username TEXT NOT NULL);""")
+    cur.execute("CREATE TABLE IF NOT EXISTS users(id INT PRIMARY KEY, username TEXT NOT NULL)")
 
-	con.commit()
-	con.close()
+    cur.execute("""CREATE TABLE IF NOT EXISTS free_recall(
+        id INT PRIMARY KEY, 
+        user_id INT, 
+        prompt TEXT NOT NULL, 
+        source TEXT NOT NULL, 
+        tag TEXT, 
+        creation_date DATETIME NOT NULL, 
+        days_till_next_recall INTEGER NOT NULL, 
+        next_recall_date DATETIME NOT NULL,
+        FOREIGN KEY(user_id) REFERENCES users(id))""")
+
+    con.commit()
+    con.close()
+
 
 def insert_profile_into_database(id, username):
     try:

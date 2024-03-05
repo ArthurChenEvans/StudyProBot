3d97e8faa257d1ebb59166162aae1bff3d25dffc
ArthurChenEvans <arthurchenevans@protonmail.com>
2024-03-05 14:35:21 +0000

    Fixed database issues

--- db.py
+++ db.py
@@ -4,8 +4,28 @@ def setup_database():
 	con = sqlite3.connect("discord.db")
 	cur = con.cursor()
 
-	cur.execute("""CREATE TABLE IF NOT EXISTS users("id INTEGER PRIMARY KEY, username TEXT NOT NULL")""")
+	cur.execute("""CREATE TABLE IF NOT EXISTS users(id INT PRIMARY KEY, username TEXT NOT NULL);""")
 
 	con.commit()
 	con.close()
 
+def insert_profile_into_database(id, username):
+    try:
+        con = sqlite3.connect("discord.db")
+        cur = con.cursor()
+
+        cur.execute("""SELECT id FROM users WHERE id = ?""", (id,))
+        user_exists = cur.fetchone()
+
+        if user_exists:
+            return False
+        else:
+            cur.execute("""INSERT INTO users (id, username) VALUES(?, ?)""", (id, username))
+            con.commit()
+            return True
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        return False
+    finally:
+        if con:
+            con.close()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import db as database
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
@@ -11,10 +12,20 @@ intents.message_content = True
 
 bot = discord.Bot()
 
+profiles = bot.create_group("profile", "Create/Read/Update/Delete commands for user profile/s")
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
     database.setup_database()
+
+@profiles.command(description="Creates a profile to keep track of retrieval!")
+async def create(ctx):
+    success = database.insert_profile_into_database(ctx.author.id, ctx.author.name)
+    if success:
+        await ctx.respond(f"Succesfully created a profile for {ctx.author}!")
+    else:
+        await ctx.respond(f"User profile already exists for {ctx.author} or a database error occured.")
 
 
 bot.run(DISCORD_TOKEN)
